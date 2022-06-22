87d60b0298ef9e39725d57d3bd4784c0a9ccbe91
aadnyak <92675097+aadnyak@users.noreply.github.com>
2022-06-22 23:05:26 +0530

    Add files via upload

---
+++ practice/student.java
@@ -0,0 +1,23 @@
+package aadnya; 
+
+public class student {
+	
+	 static void main()
+	 {
+		 System.out.println("Hello");
+	 }
+	  void display()
+	  {
+		  
+	  }
+	public static void main(String []abc) { 
+	
+	System.out.println("Hello java");
+	
+	student obj=new student();
+	
+	obj.display();
+	
+	}
+}
+
